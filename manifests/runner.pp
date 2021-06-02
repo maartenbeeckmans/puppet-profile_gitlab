fed298e09408034d3ea280b550fd186e5e9ce95f
Maarten Beeckmans <maartenb@inuits.eu>
2021-06-02 22:54:16 +0200

    fix typos and add option to register runner

--- manifests/runner.pp
+++ manifests/runner.pp
@@ -3,18 +3,22 @@
 #
 class profile_gitlab::runner (
   Boolean                                    $manage_repo,
-  Boolean                                    $repo_name,
+  String                                     $repo_name,
   Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $repo_location,
   Hash                                       $repo_key,
   String                                     $package_name,
   String                                     $package_ensure,
   Boolean                                    $manage_pin,
+  Boolean                                    $register,
+  Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $gitlab_url,
+  String                                     $gitlab_token,
 ) {
   if $manage_repo {
     apt::source { $repo_name:
       comment  => 'Gitlab runner configuration',
       location => $repo_location,
       key      => $repo_key,
+      before   => Package[$package_name],
     }
   }
 
@@ -32,9 +36,17 @@ class profile_gitlab::runner (
   if $manage_pin {
     apt::pin { 'gitlab-runner-pin':
       explanation => 'Prefer GitLab provided packages over Debian native ones',
-      package     => $package_name,
+      packages    => $package_name,
       origin      => $repo_location,
       priority    => 1001,
+    }
+  }
+
+  if $register {
+    exec { 'registering_runner':
+      command    => "/usr/bin/gitlab-runner register --non-interactive --url ${gitlab_url} --registration-token ${gitlab_token} --executor docker --docker-image alpine:latest --description ${facts[networking][fqdn]} --tag-list puppet_managed --run-untagged=true --locked=false --access-level=not_protected && touch /etc/gitlab-runner/registered",
+      creates    => '/etc/gitlab-runner/registered',
+      subscribe  => Package[$package_name],
     }
   }
 }
