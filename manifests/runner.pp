ebfb3a868abc9416f5e682264b43ed7dca7fbaf3
Maarten Beeckmans <maartenb@inuits.eu>
2021-08-21 18:04:46 +0200

    Add working runner configuration
    Use hashes for tokens
    Add default options
    Fix metrics consul exporter

--- manifests/runner.pp
+++ manifests/runner.pp
@@ -2,12 +2,12 @@
 #
 #
 class profile_gitlab::runner (
+  Hash                                       $runners,
+  Hash                                       $runner_defaults,
   Boolean                                    $manage_repo,
   Stdlib::IP::Address                        $gitlab_runner_exporter_listen_address,
   Stdlib::Port                               $gitlab_runner_exporter_port,
   Array                                      $gitlab_runner_exporter_tags,
-  Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $gitlab_url,
-  String                                     $gitlab_token,
   Boolean                                    $manage_sd_service                     = lookup('manage_sd_service', Boolean, first, true),
 ) {
   file { '/etc/profile.d/gitlab_runner.sh':
@@ -18,40 +18,38 @@ class profile_gitlab::runner (
   }
 
   class { 'gitlab_ci_runner':
-    concurrent     => $facts['processors']['count'],
-    log_level      => 'info',
-    log_format     => 'text',
-    check_interval => 1,
-    listen_address => "${gitlab_runner_exporter_listen_address}:${gitlab_runner_exporter_port}",
-    manage_docker  => false,
-    manage_repo    => $manage_repo,
+    runners         => $runners,
+    runner_defaults => $runner_defaults,
+    concurrent      => $facts['processors']['count'],
+    log_level       => 'info',
+    check_interval  => 1,
+    listen_address  => "${gitlab_runner_exporter_listen_address}:${gitlab_runner_exporter_port}",
+    manage_docker   => false,
+    manage_repo     => $manage_repo,
+  }
+
+  # Add custom config for session timeout
+  $_session_server_options = {
+    'session_server' => {
+      'session_timeout' => 1800,
+    }
+  }
+  concat::fragment { "${::gitlab_ci_runner::config_path} - session timeout":
+    target  => $::gitlab_ci_runner::config_path,
+    order   => 1,
+    content => gitlab_ci_runner::to_toml($_session_server_options),
   }
 
   if $manage_sd_service {
     consul::service { 'gitlab-runner':
       checks => [
         {
-          http     => "http://${gitlab_runner_exporter_listen_address}:${gitlab_runner_exporter_port}",
+          http     => "http://${gitlab_runner_exporter_listen_address}:${gitlab_runner_exporter_port}/metrics",
           interval => '10s'
         }
       ],
       port   => $gitlab_runner_exporter_port,
       tags   => $gitlab_runner_exporter_tags,
     }
-  }
-
-  gitlab_ci_runner::runner { 'runner':
-    ensure  => present,
-    config  => {
-      url      => $gitlab_url,
-      token    => $gitlab_token,
-      executor => 'docker',
-      limit    => $facts['processors']['count'],
-      docker   => {
-        image => 'alpine:latest',
-      },
-    },
-    require => Class['gitlab_ci_runner::config'],
-    notify  => Class['gitlab_ci_runner::service'],
   }
 }
